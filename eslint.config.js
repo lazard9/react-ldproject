ce34e5c380a2b151fd2e0947546ee320af5ac1e7
Lazar Dacic <lazar.dacic@gmail.com>
2025-08-06 00:25:18 +0200

    chore(eslint): fix config and add jsx-a11y and testing-library plugins for accessibility and testing rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@
 import js from "@eslint/js";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import testingLibrary from "eslint-plugin-testing-library";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
@@ -10,8 +13,15 @@ export default [
     plugins: {
       react,
       "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+      "testing-library": testingLibrary,
     },
     languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
       ecmaVersion: "latest",
       sourceType: "module",
       parserOptions: {
@@ -26,12 +36,14 @@ export default [
       },
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      ...testingLibrary.configs.react.rules,
+
       "react/react-in-jsx-scope": "off",
       "react/jsx-uses-react": "off",
       "react/prop-types": "off",
-
-      ...react.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
     },
   },
 ];
